0c4e0596eacfd4af92bf39a5a76d1ec3abff4ab5
Adnan <Adnan.theentertainer@gmail.com>
2019-09-12 19:31:20 +0500

    firebase dependencies added

--- app/src/main/java/com/spaktask/MainActivity.java
+++ app/src/main/java/com/spaktask/MainActivity.java
@@ -1,14 +1,21 @@
 package com.spaktask;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.spaktask.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Binding component instance
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
+
 }
